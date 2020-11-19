f8d03615953f3e0a462c63aafc10b729fc08904b
NaimaSiddiqui <70418243+NaimaSiddiqui@users.noreply.github.com>
2020-11-19 12:57:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,22 +4,24 @@ var thickness;
 
 function setup() {
   createCanvas(1600,400);
+  
+  thickness=random(22,83)
+ speed=random(223,321);
+ weight=random(30,52);
+  
  bullet=createSprite(50, 200, 50, 25);
  bullet.shapeColor = "white";
  wall=createSprite(1200,200,thickness, height/2);
  wall.shapeColor = (80, 80, 80);
- thickness=random(22,83)
- speed=random(223,321);
- weight=random(30,52);
+   bullet.velocityX = speed;
 }
 
 function draw() {
   background(0,0,0);  
-  bullet.velocityX = speed;
+//  bullet.velocityX = speed;
 
-if(wall.x-bullet.x<(bullet.width+wall.width/2))
-{
-bullet.velocityX=0;
+
+//bullet.velocityX=0;
 
 var damage = 0.5 * weight * speed* speed/22509;
 if(deformation>180)
@@ -36,7 +38,7 @@ if(damage>100)
   wall.shapeColor=("green");
   
 }
- }
+ 
 
   drawSprites();
 }
